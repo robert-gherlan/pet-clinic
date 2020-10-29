68b45717a769228448d3e35a4d6165a9cb3b08c9
robertgherlan@gmail.com <Robert.1994>
2020-10-30 00:37:37 +0200

    Added vets JSON API endpoint.

--- pet-clinic-web/src/main/java/com/gherlan/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/gherlan/petclinic/controllers/VetController.java
@@ -1,11 +1,15 @@
 package com.gherlan.petclinic.controllers;
 
+import com.gherlan.petclinic.model.Vet;
 import com.gherlan.petclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Controller
@@ -18,5 +22,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/v1/vets")
+    public ResponseEntity<Set<Vet>> getVets() {
+        return ResponseEntity.ok(vetService.findAll());
     }
 }
